2330bcad0e7a5f308863bc64ab47f0d6b3564691
Ludovic DEDIER <ldedier@e2r5p14.42.fr>
2018-11-28 21:10:39 +0100

    ouuai

--- srcs/main.c
+++ srcs/main.c
@@ -6,12 +6,11 @@
 /*   By: ldedier <ldedier@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/21 13:42:56 by ldedier           #+#    #+#             */
-/*   Updated: 2018/11/28 17:44:46 by ldedier          ###   ########.fr       */
+/*   Updated: 2018/11/28 21:04:23 by ldedier          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
-
 /*
 void __attribute__((destructor)) end();
 
@@ -21,7 +20,6 @@ void	end(void)
 	while(1);
 }
 */
-
 void	ft_print_main_error(int ret)
 {
 	if (ret == 2)
